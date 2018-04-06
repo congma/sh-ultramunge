e188abdc9b1f759d0eddc57cc3330599604632d3
Cong Ma <cong.ma@obspm.fr>
2018-04-06 17:57:19 +0800

    tests: use double-quote busting in injection attempt

--- tests/f/test_command_like_in_seq.sh
+++ tests/f/test_command_like_in_seq.sh
@@ -1,5 +1,5 @@
 echo 'TEST: sneaky syntax in the old value of sequence variable'
 rm -rf test_yyy
-SOME_VAR='/bin:$(mkdir test_yyy)'
+SOME_VAR='/bin:";mkdir test_yyy'
 ultramunge thing SOME_VAR after
 [ -d test_yyy ]
