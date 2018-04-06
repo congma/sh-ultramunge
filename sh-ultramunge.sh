6287f0d8d000fefb72f32831a2696f610ecff4d3
Cong Ma <cong.ma@obspm.fr>
2018-04-06 17:23:20 +0800

    code generation: encode user-provided data to prevent quote injection
    Prevent injection of quote characters in `eval`-ed source code by
    base64-encoding possibly unclean live data.  The encoded data has a
    limited alphabet that contains only [A-Za-z0-9+/].  Therefore, the
    expansion cannot contain the enclosing quotation-mark characters, making
    it impossible to terminate a quote-surrounded unit prematurely.
    
    This removes the restriction on the allowed characters.

--- sh-ultramunge.sh
+++ sh-ultramunge.sh
@@ -5,39 +5,53 @@
 # shell environment variable NAME.
 ultramunge ()
 {
-    path_value="$1"
+    _UM_ITEM="$1"
     # Skip entire operation if the value to be added is empty.
-    if [ -z "$path_value" ]; then
+    if [ -z "${_UM_ITEM}" ]; then
 	return 0
     fi
-    variable_name="$2"
+    _UM_VNAME="$2"
     # Valid name must comform to the POSIX standard for environment variables
     # This also filters out sneaky names.
-    if ! echo "${variable_name}" | grep -q '^[A-Z_][A-Z0-9_]*$'; then
+    if ! echo "${_UM_VNAME}" | grep -q '^[A-Z_][A-Z0-9_]*$'; then
 	return 1
     fi
     # Dereference the expanded value of the named variable.
     # Similar to the Bash statement
-    #     variable_value="${!variable_name}"
-    eval "variable_value=\"\${${variable_name}}\""
-    # Prevent injection by closing the single quote. Reject all values
-    # containing the single quote character.
+    #     _UM_VVALUE="${!_UM_VNAME}"
+    eval "_UM_VVALUE=\"\${${_UM_VNAME}}\""
+    # If the mungee variable is already empty or non-set, just set it to the
+    # given item.
     # shellcheck disable=SC2154
-    if echo "${variable_value}""${path_value}" | grep -q "'"; then
-	return 1
-    fi
-    # Set the named variable to the appended value.
-    # shellcheck disable=SC2154
-    if [ -z "${variable_value}" ]; then
-	eval "${variable_name}='${path_value}'"
+    if [ -z "${_UM_VVALUE}" ]; then
+	_UM_HEAD="$( printf '%s' "${_UM_ITEM}" | base64 )"
+	# shellcheck disable=SC2016
+	_UM_SUBSTR='"$( printf "${_UM_HEAD}" | base64 --decode )"'
+	eval "${_UM_VNAME}=${_UM_SUBSTR}"
     else
-	pattern="(^|:)${path_value}($|:)"
-	if ! echo "$variable_value" | grep -Eq "$pattern"; then
+	_UM_PATTERN="(^|:)${_UM_ITEM}($|:)"
+	# Test if the variable already contains the value as part.
+	if ! echo "$_UM_VVALUE" | grep -Eq "${_UM_PATTERN}"; then
+	    # Guarantee that neither the head nor the tail variables expands
+	    # to anything containing the single-quote (') character.
 	    if [ "$3" = after ]; then
-		eval "${variable_name}='${variable_value%:}:${path_value}'"
+		_UM_HEAD="$( printf '%s' "${_UM_VVALUE%:}" | base64 )"
+		_UM_TAIL="$( printf '%s' "${_UM_ITEM}" | base64 )"
 	    else
-		eval "${variable_name}='${path_value}:${variable_value#:}'"
+		# shellcheck disable=SC2034
+		_UM_HEAD="$( printf '%s' "${_UM_ITEM}" | base64 )"
+		# shellcheck disable=SC2034
+		_UM_TAIL="$( printf '%s' "${_UM_VVALUE#:}" | base64 )"
 	    fi
+	    # The only user-expandable part of the following command string
+	    # are encoded such that the alphabet is not large enough to contain
+	    # characters necessary for code-injection.
+	    # shellcheck disable=SC2016
+	    _UM_SUBSTR='"$( printf "%s" "${_UM_HEAD}" | base64 --decode )":"$( printf "%s" "${_UM_TAIL}" | base64 --decode )"'
+	    eval "${_UM_VNAME}=${_UM_SUBSTR}"
 	fi
+	# End of "test if the variable already contains the value as part".
     fi
+    unset -v _UM_ITEM _UM_VNAME _UM_VVALUE _UM_HEAD _UM_TAIL _UM_SUBSTR \
+	     _UM_PATTERN
 }
